cd4f5323a425ef88d19079f23c7a5ec7531be9a5
hwangjamie <huangjueming@caiji.live>
2021-04-25 11:18:35 +0800

    init🎉

---
+++ src/views/axios.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "../utils/axios";
+
+export default defineComponent({
+  setup() {
+    axios
+      .get("/user/XPost")
+      .then((res) => {
+        console.log("res:", res);
+      })
+      .catch((err) => {
+        console.log("err:", err);
+      });
+  },
+});
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">Hello World!</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  background: red;
+}
+</style>
